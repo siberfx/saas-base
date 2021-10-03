32f561c95405f8bb174291fc40213c26b9c00cf6
Selim GORMUS(gm) <info@siberfx.com>
2021-10-03 16:52:33 +0300

    massive fixes applied

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 namespace Database\Seeders;
+
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -11,8 +12,8 @@ class UsersTableSeeder extends Seeder
             [
                 'id'             => 1,
                 'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => '$2y$10$1g1u0eStm.kthizHN.Kmaet7aO1KFNvsYnL1vFSxqIdMXEsdg11Gu',
+                'email'          => 'info@siberfx.com',
+                'password'       => bcrypt('47819812'),
                 'remember_token' => null,
             ],
         ];
